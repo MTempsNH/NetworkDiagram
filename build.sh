b6a1037b9a4d0e4c85c44f61d6dc38a63a6d59ee
Mark Temple <mtemps@gmail.com>
2017-10-23 16:05:16 -0400

    trouble-shooting dist directory issues

--- build.sh
+++ build.sh
@@ -14,3 +14,4 @@ echo '----------------- Attempting to zip project structure...'
 zip -r dist/fusion-api-jenkinsfiles.zip dist node_modules server.js package.json
 echo '----------------- Copying Fusionfile to dist folder...'
 cp Fusionfile dist
+cp /dist dist
