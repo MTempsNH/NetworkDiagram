0c1787dddd348b967225f75218adaa0ee2e5484c
Mark Temple <mtemps@gmail.com>
2017-10-24 09:22:17 -0400

    remove dist copy - 2

--- build.sh
+++ build.sh
@@ -14,5 +14,5 @@ echo '----------------- Attempting to zip project structure...'
 zip -r dist/fusion-api-jenkinsfiles.zip dist node_modules server.js package.json
 echo '----------------- Copying Fusionfile to dist folder...'
 cp Fusionfile dist
-mkdir -p dist/dist
+mkdir -p /dist/dist
 
